a1d369c9f6c3e4b99dc8af74ce7ce561be516be3
Aymone SOH <aymonecynthiasoh@gmail.com>
2024-02-05 22:30:08 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,21 +1,33 @@
-import express from 'express'; 
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+dotenv.config()
 
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
-        console.log('Connected to MongoDB!');
+        console.log('Connected to MongoDB!')
     })
     .catch((err) => {
-        console.log(err);
+        console.log(err)
     })
 
-const app = express();
+const app = express()
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+    console.log('Server is running on port 3000!')
     }
-);
+)
 
+app.get('/test', (req, res) => {
+    res.json({
+        message: 'Hello World!',
+    })
+})
+
+//Toutes les routes doivent être définies dans ce fichier.
+
+
+//Chaque fois que quelqu'un ira sur la route /api/user, il va checker toutes les routes de userRouter. Et la première d'entre elles est /test. Pour avoir le message, il faudra donc mettre le chemin api/user/test dans l'url.
+app.use("/api/user", userRouter)
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,8 +15,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
-}, { timestamps: true });
+}, { timestamps: true })
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
