715d5ce69cd2a82c4c3f7355d38fc002cafbf8b2
Aymone SOH <aymonecynthiasoh@gmail.com>
2024-02-09 17:19:15 +0100

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+
 dotenv.config()
 
 mongoose
@@ -37,6 +39,7 @@ app.get('/test', (req, res) => {
 //Chaque fois que quelqu'un ira sur la route /api/user, il va checker toutes les routes de userRouter. Et la première d'entre elles est /test. Pour avoir le message, il faudra donc mettre le chemin api/user/test dans l'url.
 app.use("/api/user", userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 app.use((err, req, res, next ) => {
     const statusCode =  err.statusCode || 500
